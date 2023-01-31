b4cf54e553de864e87802b6b031ffc719928f4fd
Antonio Alonso <antonioam@live.com>
2023-01-31 18:44:25 -0500

    Create Source.cpp
    initial commit

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
+
+int nScreenWidth = 80;   // Default screen size
+int nScreenHeight = 30;
+
+int Rotate(int px, int py, int r){
+    switch (r%4){
+        case 0: return px + (py*4);    // 0 degrees
+	case 1: return 12+py - (px*4); // 90 degrees
+	case 2: return 15-px - (py*4); // 180 degrees
+	case 3: return 3-py + (px*4);  // 270 degrees
+    }
+}
+
+int main(){
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"....");
+    tetromino[1].append(L"....");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".XX.");
+
+    tetromino[2].append(L"....");
+    tetromino[2].append(L"....");
+    tetromino[2].append(L"...X");
+    tetromino[2].append(L".XXX");
+    
+    tetromino[3].append(L"....");
+    tetromino[3].append(L"....");
+    tetromino[3].append(L"X...");
+    tetromino[3].append(L"XXX.");
+
+    tetromino[4].append(L"....");
+    tetromino[4].append(L"....");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"XX..");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..XX");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L"....");
+    tetromino[6].append(L"..X.");
+    tetromino[6].append(L".XXX");
+
+
+    pField = new unsigned char[nFieldWidth*nFieldHeight];
+    for (int x = 0; x < nFieldWidth; x++)
+	for (int y = 0; y < nFieldHeight; y++)
+	    pField[y*nFieldWidth + x] = (x==0 || x==(nFieldWidth-1) || y == (nFieldHeight-1)) ? 9 : 0;
+
+
+    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+    for (int i = 0; i > nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+
+    bGameOver = false;
+    while (!bGameOver){
+
+
+	// Draw Field
+	for (int x = 0; x < nFieldWidth; x++)
+	    for (int y = 0; y < nFieldHeight; y++)
+		screen[(y+2)*nScreenWidth + x+2] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]];
+
+
+    	// Display Frame
+    	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0,0}, &dwBytesWritten);
+    }
+    return 0;
+}
